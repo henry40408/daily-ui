3ef89ceacad1f2af9b18e62e5638dc3445913353
Heng-Yi Wu <henry40408@gmail.com>
2016-10-30 15:37:43 +0800

    detect card type when typing card number

--- source/javascripts/2.js
+++ source/javascripts/2.js
@@ -1,16 +1,28 @@
 'use strict';
 
 jQuery(document).ready(function () {
+  var VISA = /^4[0-9]{6,}$/;
+  var MASTER_CARD = /^5[1-5][0-9]{5,}|222[1-9][0-9]{3,}|22[3-9][0-9]{4,}|2[3-6][0-9]{5,}|27[01][0-9]{4,}|2720[0-9]{3,}$/;
+  var AMERICAN_EXPRESS = /^3[47][0-9]{5,}$/;
+  var DINERS_CLUB = /^3(?:0[0-5]|[68][0-9])[0-9]{4,}$/;
+  var DISCOVER = /^6(?:011|5[0-9]{2})[0-9]{3,}$/;
+  var JCB = /^(?:2131|1800|35[0-9]{3})[0-9]{3,}$/;
+
   var $number = jQuery('#card-number');
   var $expireOn = jQuery('#expire-on');
   var $cardHolder = jQuery('#card-holder');
   var $cvv = jQuery('#cvv');
+  var $paymentIcon = jQuery('#payment-icon');
 
   var $expireMonthField = jQuery('#expire-month-field');
   var $expireYearField = jQuery('#expire-year-field');
   var $numberField = jQuery('#card-number-field');
   var $cardHolderField = jQuery('#card-holder-field');
   var $cvvField = jQuery('#cvv-field');
+
+  function setPaymentIcon(type) {
+    $paymentIcon.empty().html($('<i>').addClass('pf pf-' + type));
+  }
 
   Rx.Observable.fromEvent($numberField, 'input').map(function (e) {
     return e.target.value;
@@ -22,6 +34,16 @@ jQuery(document).ready(function () {
     }, '');
   }).do(function (v) {
     return $numberField.val(v);
+  }).do(function(v) {
+    switch (true) {
+      case VISA.test(v): setPaymentIcon('visa'); break;
+      case MASTER_CARD.test(v): setPaymentIcon('mastercard'); break;
+      case AMERICAN_EXPRESS.test(v): setPaymentIcon('american-express'); break;
+      case DINERS_CLUB.test(v): setPaymentIcon('diners'); break;
+      case DISCOVER.test(v): setPaymentIcon('discover'); break;
+      case JCB.test(v): setPaymentIcon('jcb'); break;
+      default: $paymentIcon.empty();
+    }
   }).map(function (v) {
     return (v + '________________').slice(0, 16);
   }).map(function (v) {
